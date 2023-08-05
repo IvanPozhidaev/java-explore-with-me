cad52c554f23a8e6055edacbb133be8a6fb97ad5
Ivan Pozhidaev <craig1408@yandex.ru>
2023-08-05 09:25:41 +0700

    fix: HitModel - убрать аннотацию @Data

--- stats/server/src/main/java/ru/practicum/ewm/stats/model/HitModel.java
+++ stats/server/src/main/java/ru/practicum/ewm/stats/model/HitModel.java
@@ -1,30 +1,31 @@
 package ru.practicum.ewm.stats.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity
 @AllArgsConstructor
 @RequiredArgsConstructor
 @NoArgsConstructor(force = true)
 @Table(name = "hits", schema = "public")
 public class HitModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private final Long id;
+
     @Column(name = "application", nullable = false)
     private String app;
+
     @Column(name = "uri", nullable = false)
     private String uri;
+
     @Column(name = "remote_ip", nullable = false)
     private String ip;
+
     @Column(name = "date_time", nullable = false)
     private LocalDateTime timestamp;
 }
